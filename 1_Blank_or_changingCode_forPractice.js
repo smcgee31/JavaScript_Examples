6de08240fadd1535bb0d1138d0065bb9d3882c5b
smcgee31 <solodesign31@gmail.com>
2016-06-24 22:21:48 -0600

    added toy problems

--- 1_Blank_or_changingCode_forPractice.js
+++ 1_Blank_or_changingCode_forPractice.js
@@ -1,22 +1,26 @@
-// Your task is to make a function that can take any non-negative integer as a argument
-// and return it with it's digits in descending order. Descending order means that you
-// take the highest digit and place the next highest digit immediately after it.
+var list = {value: 1, next: {value: 2, next: {value: 3, next: null}}};
 
-var num = 1254859723;
+// Solution #1
+var listToArray = (list) => {
+  var arr = [];
+  for (var prop in list) {
+    var value = list[prop];
+    if (typeof value === 'object') arr.push(...listToArray(value));
+    else arr.push(value);
+  }
+  return arr;
+};
 
-function descendingOrder(n){
-  n = n.toString()
-    .split('')
-    .sort(function(a,b){return b-a;})  // this line could also read .sort().reverse()
-    .join('')
-  ;
-  n = Number(n);
-  return n;
+console.log(listToArray(list));
+
+
+
+// Solution #2
+const listToArr = (obj, arr) => {
+  if (!arr) arr=[];
+  arr.push(obj.value);
+  if (obj.next) return listToArr(obj.next, arr);
+  return arr;
 }
 
-console.log(descendingOrder(num));
-
-// the same solution above could be shortened to:
-function descendingOrder(n){
-  return Number(String(n).split('').sort().reverse().join(''));  // and Number could have been replaced with parseInt
-}
+console.log(listToArr(list));
