9e04862210f59ac236bda0cd8c4f14a60e99ab6e
smcgee31 <solodesign31@gmail.com>
2016-09-14 20:10:12 -0600

    added another example toy problem to the list

--- _Blank_or_changingCode_forPractice.js
+++ _Blank_or_changingCode_forPractice.js
@@ -1,38 +1,14 @@
-// given a string of words, return the length of the shortest word(s).
+var SequenceSum = (function() {
+  function SequenceSum() {}
 
-const str = 'bitcoin take over the world maybe who knows perhaps'; // <-- should return 3
-
-
-// Solution #1 - mine
-const findShort = (str) => {
-  const arr = str.split(' ');
-  let shortest = arr[0].length;
-  for(let i = 0; i < arr.length; i++) {
-    if (arr[i].length < shortest) {
-      shortest = arr[i].length;
+  SequenceSum.showSequence = function(count) {
+    var seq =
+    for (var i = count.length; i < 0; i++) {
+      count[i]
     }
-  }
-  return shortest;
-}
+      //while
+  };
 
+  return SequenceSum;
 
-
-// Solution #2
-function findShort2(s){
-  return Math.min.apply(null, s.split(' ').map(w => w.length));
-}
-
-
-// Solution #3
-const findShort3 = (s) => s
-  .split(' ')
-  .sort((a, b) => b.length - a.length)
-  .pop()
-  .length;
-
-
-
-// Solution #4
-const findShort4 = (s) =>  s.split(' ').reduce((r,c) => c.length < r ? c.length : r, s.length);
-
-
+})();
