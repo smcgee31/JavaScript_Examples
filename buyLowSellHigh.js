333987cb85ac789b63522df155c05ae7b5492604
smcgee31 <solodesign31@gmail.com>
2016-10-05 22:26:23 -0600

    renamed one, and working on a hard one but not quite there yet... getting close!

--- buyLowSellHigh.js
+++ buyLowSellHigh.js
@@ -3,35 +3,58 @@
 //Write a solution for finding the best buy and sell point of the day for the most profit.
 // ie. buy at the low of the day and sell at the high of the day but
 // only when it makes the most profit not just any buy and sell for any profit.
-
+//               0     1     2     3     4     5     6     7    8     9     10
 var prices = [ 5.25, 5.35, 5.15, 5.35, 5.50, 5.75, 5.50, 5.45, 5.35, 5.15, 5.00, 4.85 ];
 
-function getProfit( prices ) {
-	var finalBuy = prices[0]
-    , finalBuyIndex = 0
-    , finalSell = prices[0]
-    , finalSellIndex = 0
-    , currBuy = prices[0]
-    , currBuyIndex = 0
-    , currSell = prices[0]
-    , currSellIndex = 0
+var getProfit = function( prices ) {
+	var finalBuy = prices[0]   // 5.15
+    , finalBuyIndex = 0      //  2
+    , finalSell = prices[0]  // 5.35
+    , finalSellIndex = 0     //  3
+    , currBuy = prices[0]    // 5.15
+    , currBuyIndex = 0       //  2
+    , currSell = prices[0]   // 5.50
+    , currSellIndex = 0      //  4
     , finalDiff = 0
     , currDiff = 0
   ;
 
 	// We have to compare the difference between a specified low and a newfound high
 	for ( var i = 1; i < prices.length; i++ ) {
-		if ( ( prices[ i ] > finalBuy ) && ( i > finalBuyIndex ) ) {
-			finalBuy = prices[i];
-			finalBuyIndex = i;
-		}
-		if ((prices[i] < currBuy)&&(i > currBuyIndex)) {
+		// if a newfound high sell price comes AFTER a newfound low buy price set it as the finalBuy price and record it's index
+		// each number... is it a newfound high sell price?
+		//   if so... does it come AFTER an already set low buy price? (is it's index > the currBuy)
+
+		// if the price you're looking at right now is higher than the current buy price then set currBuy to that value and currBuyIndex to that index
+		if (prices[i] < currBuy) {
 			currBuy = prices[i];
 			currBuyIndex = i;
 		}
 
+		// if the price you're looking at right now is lower than the current sell price then set currSell to that value and currSellIndex to that index
+		if (prices[i] > currSell) {
+			currSell = prices[i];
+			currSellIndex = i;
+		}
+
+		// if the price your're looking at right now is equal to the currSell && the index is greater than the currSell then set the currSellIndex to this index!
+		if (prices[i] = currSellIndex && i > currSellIndex) {
+			currSellIndex = i;
+		}
+
+		// if the currDiff > finalDiff then set currSell to finalSell and the currBuy to the finalBuy
+		if ((currSell - currBuy) > (finalSell - finalBuy) && currBuyIndex < currSellIndex) {
+			finalBuy = currBuy;
+			finalBuyIndex = currBuyIndex;
+			finalSell = currSell;
+			finalSellIndex = currSellIndex;
+		}
+
 	}
-
-
+	finalDiff = finalSell - finalBuy;
+	console.log(`finalSell price = ${finalSell} at ${finalSellIndex} hour`);
+	console.log(`resulting in a profit of ${finalDiff}`);
 
 }
+
+getProfit(prices);
